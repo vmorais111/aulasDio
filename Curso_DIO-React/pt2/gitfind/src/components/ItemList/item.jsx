df62b08d147c809db6d79edf7f0dbc66204b6109
Vitor F. Morais <111707785+vmorais111@users.noreply.github.com>
2023-10-30 12:30:16 -0300

    pt 2 - 2

---
+++ Curso_DIO-React/pt2/gitfind/src/components/ItemList/item.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles.css'
+
+function ItemList({title, description}){
+    return (
+        <div className="ItemList">
+            <strong>{title}</strong>
+            <p>{description}</p>
+            <hr />
+        </div>
+    )
+}
+
+export default ItemList;
